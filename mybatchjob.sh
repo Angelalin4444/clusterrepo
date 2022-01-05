1bfd3b2a0dda2d951e485e5260cc37452bf202e1
Angela Lin <angelalin@angelas-air.attlocal.net>
2022-01-05 13:41:52 -0600

    fixed username

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --partition=sched_mit_sloan_batch
 #SBATCH --time=0-00:10
-#SBATCH -o /home/Angelalin4444/clusterrepo/myoutputfile.out
-#SBATCH -e /home/Angelalin4444/clusterrepo/myerrorfile.err
+#SBATCH -o /home/aglin/clusterrepo/myoutputfile.out
+#SBATCH -e /home/aglin/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=angelalin4444@gmail.com
 
